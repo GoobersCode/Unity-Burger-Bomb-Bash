a8a6072c319986b23570e00463772c2ff17d8f84
GoobersCode <evan.roderick1@gmail.com>
2020-10-13 13:34:57 -0500

    Added Base Unity Project

---
+++ Dummy/Assets/Scripts/Cannon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    [SerializeField] GameObject projectilePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform projectile = Instantiate(projectilePrefab, new Vector3(-1.6f, -1f, 0), Quaternion.identity).transform;
+        projectile.transform.parent = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        
+    }
+}
